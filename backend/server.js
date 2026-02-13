0eed799d93bbe52cc1f18b61b5e51809fb84717e
Satyam Jain <154873944+Satyamsj@users.noreply.github.com>
2026-02-13 22:32:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -91,4 +91,6 @@ app.post("/api/login", (req, res) => {
 });
 //  SERVER START
 
-app.listen(5000, "0.0.0.0", () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
