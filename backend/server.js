c054a606922f77b6322f437e078dcfd8855d6aef
Satyam Jain <154873944+Satyamsj@users.noreply.github.com>
2026-02-14 01:15:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { Pool } = require("pg");
 
 const app = express();
 app.use(cors({
-  origin: "https://your-frontend-domain.com", // replace with your frontend URL
+  origin: "https://bus-management-system-with-ai-imple.vercel.app/", // replace with your frontend URL
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
